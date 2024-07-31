32c2408e4053b3a22334be54606e4d041a9b3c50
joyfranky2309 <joyfranksebastian23@gmail.com>
2024-07-31 11:22:59 +0530

    pop

--- server/Brain/Dimaag.js
+++ server/Brain/Dimaag.js
@@ -37,7 +37,7 @@ async function correction(prompt,response){
       "messages": [
         {
           "role": "user",
-          "content": "your name is Levi Ackerman and you are a legal assistant enhance the given response: "+ response+" appropriately based on the given question: "+prompt+" based on indian law And you don't have to specify your name unless the question is about your name ",
+          "content": "your name is Levi Ackerman and you are a legal assistant enhance the given response: "+ response+" appropriately based on the given question: "+prompt+" based on indian law And you don't have to specify your name unless the question is about your name ,include the appropriate sections,laws and article wherever necessary.",
         }
       ],
       "model": "llama3-70b-8192",
